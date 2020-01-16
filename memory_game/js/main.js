afb310b4f9653385e2c539d92ec96c0358697ec2
Vincent Kokojan <vincentkokojan@vincents-mbp.attlocal.net>
2020-01-15 19:38:54 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,14 @@
-console.log("Up and running!");
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
+const cardOne = cards[0];
+const cardTwo = cards[2];
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+cardsInPlay;
+if (cardsInPlay.length === 2);
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+}
+	else {
+	alert("Sorry, try again.");
+};
